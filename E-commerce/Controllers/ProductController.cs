35096bd9fe2b7a28601d67fb8342c1b19a48ccec
BuildTools <unconfigured@null.spigotmc.org>
2024-09-29 19:04:25 +0700

    Add To Cart Ajax, Searches, Related products

--- E-commerce/Controllers/ProductController.cs
+++ E-commerce/Controllers/ProductController.cs
@@ -1,10 +1,11 @@
 ﻿using E_commerce.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_commerce.Controllers
 {
-    public class ProductController : Controller
-    {
+	public class ProductController : Controller
+	{
 		private readonly DataContext _dataContext;
 		public ProductController(DataContext context)
 		{
@@ -14,11 +15,23 @@ namespace E_commerce.Controllers
 		{
 			return View();
 		}
+		public async Task<IActionResult> Search(string searchTerm)
+		{
+			var products = await _dataContext.Products.Where(p => p.Name.Contains(searchTerm) || p.Description.Contains(searchTerm)).ToListAsync();
+			ViewBag.Keyword = searchTerm;
+
+			return View(products);
+		}
 		public async Task<IActionResult> Details(long Id)
 		{
 			if (Id == null) return RedirectToAction("Index");
 
 			var productsById = _dataContext.Products.Where(p => p.Id == Id).FirstOrDefault();
+			//related
+			var relatedProducts = await _dataContext.Products.Where(p => p.CategoryId == productsById.CategoryId && p.Id != productsById.Id)
+				.Take(4)
+				.ToListAsync();
+			ViewBag.RelatedProducts = relatedProducts;
 
 			return View(productsById);
 		}
