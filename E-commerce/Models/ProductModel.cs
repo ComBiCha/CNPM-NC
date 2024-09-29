a507f21510c083ee7ecb9ee1cc086b68dc2b7858
BuildTools <unconfigured@null.spigotmc.org>
2024-09-29 20:51:04 +0700

    Rating part 1

--- E-commerce/Models/ProductModel.cs
+++ E-commerce/Models/ProductModel.cs
@@ -23,6 +23,7 @@ namespace E_commerce.Models
         public int CategoryId { get; set; }
 		public CategoryModel Category { get; set; }
 		public BrandModel Brand { get; set; }
+		public RatingModel Ratings { get; set; }
 		public string Image { get; set; } = "noimage.jpg";
 
 		[NotMapped]
